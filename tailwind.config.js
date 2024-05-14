3be70a646245944bad02bcef80af328c7165e938
hadiminayicleargit commit -m project-version-1 <hadiminayi@gmail.com>
2024-05-14 20:51:24 +0330

    filimo update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,7 @@ module.exports = {
       display:['Iran','sans-serif']
     },
     extend: {
-      keyframes:{
-        fadein:{
-          "0%":{opacity:0},
-          "100%":{opacity:100},
-        },
-      },
-      animation:{
-        fadein:"fadeIn 0.2s ease-in-out forwards",
-      }
+      
     },
   },
   plugins: [],
